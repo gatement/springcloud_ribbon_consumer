5da4cebad23a623dfbbb5f0cf07520e25d30e7be
Johnson Lau <gatement@gmail.com>
2018-02-21 08:50:45 +0800

    go

---
+++ src/main/java/com/lgh/springcloud/ribbon/consumer/HelloService.java
@@ -0,0 +1,33 @@
+package com.lgh.springcloud.ribbon.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class HelloService {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@HystrixCommand(fallbackMethod = "helloFallback")
+	public String hello() {
+		long start = System.currentTimeMillis();
+
+		String result = restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+
+		long end = System.currentTimeMillis();
+		log.info("Spend time: {}", end - start);
+
+		return result;
+	}
+
+	public String helloFallback() {
+		return "error";
+	}
+}
