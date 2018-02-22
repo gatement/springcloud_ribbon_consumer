b2b8ea01aae3f4b668025085d4390c55fa57891f
Johnson Lau <gatement@gmail.com>
2018-02-22 22:27:26 +0800

    go

--- src/main/java/com/lgh/springcloud/ribbon/consumer/controller/ConsumerController.java
+++ src/main/java/com/lgh/springcloud/ribbon/consumer/controller/ConsumerController.java
@@ -1,9 +1,11 @@
-package com.lgh.springcloud.ribbon.consumer;
+package com.lgh.springcloud.ribbon.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lgh.springcloud.ribbon.consumer.service.HelloService;
 
 @RestController
 public class ConsumerController {
--- src/main/java/com/lgh/springcloud/ribbon/consumer/service/HelloService.java
+++ src/main/java/com/lgh/springcloud/ribbon/consumer/service/HelloService.java
@@ -1,4 +1,4 @@
-package com.lgh.springcloud.ribbon.consumer;
+package com.lgh.springcloud.ribbon.consumer.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
